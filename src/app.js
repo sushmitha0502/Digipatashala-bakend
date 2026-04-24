ca793046815bfb0e5f790c9a6d43cd658739635d
Sushmitha <sushmithabotcha37@gmail.com>
2026-04-24 20:20:20 +0530

    fixed cors

--- src/app.js
+++ src/app.js
@@ -5,9 +5,13 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 app.use(cors({
-  origin: "https://digipatashala-frontend.vercel.app",
+  origin: [
+    "http://localhost:5173",
+    "https://digipatashala-frontend.vercel.app"
+  ],
   credentials: true,
 }));
+app.options("*", cors());
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));

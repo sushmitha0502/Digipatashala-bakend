fe09aa68a66c98bb5f87d9e114a4966806ae5f69
Sushmitha <sushmithabotcha37@gmail.com>
2026-04-24 17:30:56 +0530

    Updated vercel.json

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import db from "./database/db.js";
 import { app } from "./app.js";
 import quizRoutes from "./routes/quiz.routes.js";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5002;
 
 console.log("DB_NAME:", process.env.DB_NAME);
 console.log("MONGO_URI:", process.env.MONGO_URI);
